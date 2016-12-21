d3929109674edbadf3c900b91345b55f0ae18d41
Nicholas Christopher <nwillc@gmail.com>
2016-12-20 22:19:23 -0500

    Transaction interface

---
+++ src/test/java/com/github/nwillc/opa/TransactionTest.java
@@ -0,0 +1,33 @@
+package com.github.nwillc.opa;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TransactionTest {
+    @Test
+    public void close() throws Exception {
+        T2 transaction1;
+        try (T2 transaction = new T2()) {
+            transaction1 = transaction;
+        }
+
+        assertThat(transaction1.rollback).isTrue();
+        assertThat(transaction1.commit).isFalse();
+    }
+
+    private class T2 implements Transaction {
+        boolean rollback = false;
+        boolean commit = false;
+
+        @Override
+        public void commit() {
+            commit = true;
+        }
+
+        @Override
+        public void rollback() {
+            rollback = true;
+        }
+    }
+}
